194b1abf63ff1a19c8bd8a21efdaa804e97e01c0
Pranavinaidu <pranavinaidu97@gmail.com>
2019-07-30 05:19:50 +0530

    third program

---
+++ VowelConsonant.py
@@ -0,0 +1,8 @@
+ch = input("Enter a character")
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    if(ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'or ch=='A'or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+        print("vowel")
+    else:
+        print("consonant")
+else:
+    print("invalid")
